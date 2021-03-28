fc7c74be1cf6c6aa4ac857532a98168b324b239f
velizhaar <velizhaar@gmail.com>
2021-03-28 22:53:54 +0700

    edit border

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 //Items
 const title = document.querySelector(".title");
+const subtitle = document.querySelector(".info h2");
 const powder = document.querySelector(".powder img");
 const purchase = document.querySelector(".purchase");
 const description = document.querySelector(".info h3");
-const sizes = document.querySelector(".sizes");
+const shades = document.querySelector(".shades");
 
 //Moving Animation Event
 container.addEventListener("mousemove", (e) => {
@@ -19,9 +20,10 @@ container.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
   //Popout
   title.style.transform = "translateZ(150px)";
+  subtitle.style.transform = "translateZ(150px)";
   powder.style.transform = "translateZ(200px) rotateZ(-45deg)";
   description.style.transform = "translateZ(125px)";
-  sizes.style.transform = "translateZ(100px)";
+  shades.style.transform = "translateZ(100px)";
   purchase.style.transform = "translateZ(75px)";
 });
 //Animate Out
@@ -30,8 +32,9 @@ container.addEventListener("mouseleave", (e) => {
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
   //Popback
   title.style.transform = "translateZ(0px)";
+  subtitle.style.transform = "translateZ(0px)";
   powder.style.transform = "translateZ(0px) rotateZ(0deg)";
   description.style.transform = "translateZ(0px)";
-  sizes.style.transform = "translateZ(0px)";
+  shades.style.transform = "translateZ(0px)";
   purchase.style.transform = "translateZ(0px)";
 });
